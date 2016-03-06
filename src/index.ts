40f03403e3d0efc4f0f338dce7b027f463af87ec
extr0py <extr0py@extropygames.com>
2016-03-05 18:08:52 -0800

    Hook up 'Alternate' command with editing

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,20 @@ var vim = new Vim();
 var projectionLoader = new ProjectionLoader();
 var projectionResolver = new ProjectionResolver(projectionLoader);
 
+var currentAlternateFile = null;
+
 vim.on("BufEnter", (args) => {
     var currentBuffer = args.currentBuffer;
     if(currentBuffer) {
+        currentAlternateFile = projectionResolver.getAlternate(currentBuffer);
         console.log("Resolving projection for: " + currentBuffer + " | " + projectionResolver.getAlternate(currentBuffer));
     }
 });
 
 vim.addCommand("Alternate", () => {
-    vim.echo("Hello world");
+    if(currentAlternateFile)
+        vim.exec("edit " + currentAlternateFile)
+
 });
 
 console.log("command registered")
